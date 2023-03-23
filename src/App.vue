d5cfdb72dca7cffd6df0bb7d19746d84407a1ae2
zhangze-github <1183460943@qq.com>
2023-03-23 02:42:29 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script> -->
+
+<template>
+  <div class="wrapper">
+    <div class="left">
+      <input v-model="leftValue"/>
+      <div>test</div>
+    </div>
+    <div class="right">
+      <input v-model="rightValue"/>
+    </div>
+  </div>
+</template>
+<script>
+import { ref } from 'vue'
+const leftValue = ref("")
+const rightValue = ref("")
+</script>
+<style scoped lang="less">
+.wrapper{
+  display: flex;
+  align-items: center;
+}
+
+</style>
