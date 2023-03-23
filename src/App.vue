9e4a334c796043c52edebc6a5e7d1f99b20ad13c
zhangze-github <1183460943@qq.com>
2023-03-23 03:11:17 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,75 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div class="wrapper">
     <div class="left">
-      <input v-model="leftValue"/>
-      <div>test</div>
+      <div> 输入keys，以英文逗号分隔</div>
+      <a-textarea v-model:value="leftValue" />
+      <a-button class="btn" @click="add" type="primary">Add</a-button>
+      <div class="listWrapper" v-if="keysList?.length">
+        <div class="item"
+          v-for="i in keysList"
+        >
+         <div>{{ i }}</div>
+         <a-button  @click="delete" type="text" danger> 删除</a-button>
+        </div>
+      </div>
     </div>
     <div class="right">
-      <input v-model="rightValue"/>
+      <div> 输入JSON </div>
+      <a-textarea v-model:value="rightValue" rows="10"/>
     </div>
   </div>
 </template>
-<script>
+<script setup>
 import { ref } from 'vue'
+import {message} from 'ant-design-vue';
 const leftValue = ref("")
 const rightValue = ref("")
+const keysList = ref(localStorage.getItem('list') || [])
+function add(){
+  let value = leftValue.value;
+  value = value.trim();
+  if(!value){
+    message.error('请输入内容！！！')
+  }else{
+    keysList.value = [value, ...keysList.value]
+    localStorage.setItem('list', keysList.value)
+  }
+}
 </script>
 <style scoped lang="less">
-.wrapper{
+.wrapper {
+  min-height: 100vh;
   display: flex;
-  align-items: center;
-}
+  justify-content: space-around;
 
+  // align-items: center;
+  .left{
+    margin-top: 20px;
+    width: 40%;
+    .listWrapper{
+      width: 100%;
+      .item{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-top: 10px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 10px;
+        &:hover{
+          background-color: #eee;
+        }
+      }
+    }
+  }
+  .right {
+    width: 50%;
+    // padding: 20px;
+    margin-top: 20px;
+  }
+
+  .btn {
+    margin-top: 20px
+  }
+}
 </style>
