f1f795541677bc306ed29fa9da19e0429eaefe4c
zhangze-github <1183460943@qq.com>
2023-03-23 03:58:28 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -6,39 +6,56 @@ import HelloWorld from './components/HelloWorld.vue'
   <div class="wrapper">
     <div class="left">
       <div> 输入keys，以英文逗号分隔</div>
-      <a-textarea v-model:value="leftValue"  rows="6"/>
+      <a-textarea v-model:value="leftValue" rows="6" />
       <a-button class="btn" @click="add" type="primary">Add</a-button>
       <div class="listWrapper" v-if="keysList?.length">
         <div :class="['item', (selected === index) && 'select']" v-for="(i, index) in keysList" @click="select(index)">
           <div class="desc">{{ i }}</div>
-          <a-button @click="(e) => {e.stopPropagation(); deleteItem(index)}" type="text" danger> 删除</a-button>
+          <a-button @click="(e) => { e.stopPropagation(); deleteItem(index) }" type="text" danger>删除</a-button>
         </div>
       </div>
     </div>
     <div class="right">
       <div> 输入JSON </div>
       <a-textarea v-model:value="rightValue" rows="10" />
+      <a-table :dataSource="tableList" :columns="columns" v-if="tableList.length" :pagination="{hideOnSinglePage: true}" class="table"/>
     </div>
   </div>
 </template>
 <script setup>
-import { ref, toRaw } from 'vue'
+import { ref, toRaw, watch } from 'vue'
 import { message } from 'ant-design-vue';
+import {get} from 'lodash';
 const leftValue = ref("")
 const rightValue = ref("")
 let store = localStorage.getItem('list');
 const selected = ref(null)
-if(store){
-  try{
+if (store) {
+  try {
     store = JSON.parse(store)
-  }catch(e){
+  } catch (e) {
     store = localStorage.setItem('list', '');
     window.location.reload()
   }
-}else{
+} else {
   store = []
 }
 const keysList = ref(store)
+const tableList = ref([])
+const columns = [
+  {
+    title: 'KEY',
+    dataIndex: 'key',
+    key: 'key',
+  },
+  {
+    title: 'VALUE',
+    dataIndex: 'value',
+    key: 'value',
+  },
+]
+
+
 function add() {
   let value = leftValue.value;
   value = value.trim();
@@ -46,24 +63,58 @@ function add() {
     message.error('请输入内容！！！')
   } else {
     keysList.value = [value, ...keysList.value]
-    localStorage.setItem('list', JSON.stringify(toRaw(keysList.value)) )
+    localStorage.setItem('list', JSON.stringify(toRaw(keysList.value)))
+    leftValue.value = ''
+    if(selected.value !== null){
+      selected.value = selected.value + 1
+    }
   }
 }
 
-function deleteItem(index){
+function deleteItem(index) {
   keysList.value.splice(index, 1);
-  localStorage.setItem('list', JSON.stringify(toRaw(keysList.value)) )
-  if(index === selected.value){
+  localStorage.setItem('list', JSON.stringify(toRaw(keysList.value)))
+  if (index === selected.value) {
     selected.value = null;
   }
-  if(index < selected.value){
+  if (index < selected.value) {
     selected.value = selected.value - 1
   }
 }
 
-function select(index){
+function select(index) {
   selected.value = index;
 }
+
+watch([selected, rightValue] , () => {
+  let obj = {};
+  if(!rightValue.value){
+    message.error('请输入右侧 JSON')
+    tableList.value = []
+    return;
+  }
+  try{
+    obj = JSON.parse(rightValue.value);
+  }catch(e){
+    message.error('JSON 解析失败')
+    tableList.value = []
+    return;
+  }
+  let key = get(keysList.value, selected.value, '');
+  if (!key){
+    message.error('请选中一个key list')
+    tableList.value = []
+    return;
+  }
+  let returnArr = []
+  key.split(',').map(item => {
+    let value = get(obj, item);
+    if(value || (value === 0)){
+      returnArr.push({key: item, value: value})
+    }
+  })
+  tableList.value = returnArr;
+})
 
 </script>
 <style scoped lang="less">
@@ -93,12 +144,12 @@ function select(index){
           background-color: #eee;
         }
 
-        .desc{
+        .desc {
           word-break: break-all;
         }
       }
 
-      .select{
+      .select {
         border-color: rgb(90, 90, 199);
       }
     }
@@ -108,6 +159,9 @@ function select(index){
     width: 50%;
     // padding: 20px;
     margin-top: 20px;
+    .table{
+      margin-top: 20px;
+    }
   }
 
   .btn {
