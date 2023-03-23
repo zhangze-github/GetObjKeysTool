d5cb77141bfe9c4cc7f02ba67a57886da5daaa80
zhangze <1183460943@qq.com>
2023-03-23 22:31:19 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="left">
       <div> 输入keys，以英文逗号分隔， eg: a,b</div>
       <div> 支持定义多个层级，以英文句号分隔，eg: a.b</div>
-      <a-textarea v-model:value="leftValue" rows="6" />
+      <a-textarea v-model:value="leftValue" rows="6" @keydown.enter="add"/>
       <a-button class="btn" @click="add" type="primary">Add</a-button>
       <div class="listWrapper" v-if="keysList?.length">
         <div :class="['item', (selected === index) && 'select']" v-for="(i, index) in keysList" @click="select(index)">
@@ -17,16 +17,37 @@ import HelloWorld from './components/HelloWorld.vue'
       </div>
     </div>
     <div class="right">
-      <div> 输入JSON </div>
-      <a-textarea v-model:value="rightValue" rows="10" />
-      <a-table :dataSource="tableList" :columns="columns" v-if="tableList.length" :pagination="{hideOnSinglePage: true}" class="table"/>
+      <div> 输入JSON</div>
+      <a-textarea v-model:value="rightValue" rows="10"/>
+      <a-table :dataSource="tableList" :columns="columns" v-if="tableList.length"
+               :pagination="{hideOnSinglePage: true, defaultPageSize: 100}" class="table">
+
+        <template #bodyCell="{ column, record }">
+          <template v-if="column.key === 'value'">
+            <span v-if="record.value === '参数未上报'" style="color: #f40">
+              {{ record.value }}
+            </span>
+            <span v-else>
+              {{ record.value }}
+            </span>
+          </template>
+        </template>
+      </a-table>
+
     </div>
   </div>
 </template>
 <script setup>
-import { ref, toRaw, watch } from 'vue'
-import { message } from 'ant-design-vue';
+import {ref, toRaw, watch} from 'vue'
+import {message} from 'ant-design-vue';
 import {get, isUndefined, isBoolean} from 'lodash';
+
+
+message.config({
+  duration: 1,
+  maxCount: 2,
+});
+
 const leftValue = ref("")
 const rightValue = ref("")
 let store = localStorage.getItem('list');
@@ -44,6 +65,12 @@ if (store) {
 const keysList = ref(store)
 const tableList = ref([])
 const columns = [
+  {
+    title: '',
+    dataIndex: 'index',
+    key: 'index',
+    width: "18px"
+  },
   {
     title: 'KEY',
     dataIndex: 'key',
@@ -66,7 +93,7 @@ function add() {
     keysList.value = [value, ...keysList.value]
     localStorage.setItem('list', JSON.stringify(toRaw(keysList.value)))
     leftValue.value = ''
-    if(selected.value !== null){
+    if (selected.value !== null) {
       selected.value = selected.value + 1
     }
   }
@@ -87,36 +114,45 @@ function select(index) {
   selected.value = index;
 }
 
-watch([selected, rightValue] , () => {
+watch([selected, rightValue], () => {
   let obj = {};
-  if(!rightValue.value){
+  if (!rightValue.value) {
     message.error('请输入右侧 JSON')
     tableList.value = []
     return;
   }
-  try{
+  try {
     obj = JSON.parse(rightValue.value);
-  }catch(e){
+  } catch (e) {
     message.error('JSON 解析失败')
     tableList.value = []
     return;
   }
   let key = get(keysList.value, selected.value, '');
-  if (!key){
+  if (!key) {
     message.error('请选中一个key list')
     tableList.value = []
     return;
   }
   let returnArr = []
+  let i = 1;
   key.split(',').map(item => {
-    let value = get(obj, item);
-    if(!isUndefined(value)){
-      if(isBoolean(value)){
-        returnArr.push({key: item, value: JSON.stringify(value)})
-      }else{
-        returnArr.push({key: item, value: value})
-      }
+    item = item.trim();
+    if (!item) {
+      message.error('检测到有key值为空，轻检查所选keyList')
+      return;
     }
+    let value = get(obj, item);
+    if (!isUndefined(value)) {
+      if (isBoolean(value)) {
+        returnArr.push({index: i, key: item, value: JSON.stringify(value)})
+      } else {
+        returnArr.push({index: i, key: item, value: value})
+      }
+    } else {
+      returnArr.push({index: i, key: item, value: "参数未上报"})
+    }
+    i++;
   })
   tableList.value = returnArr;
 })
@@ -164,7 +200,8 @@ watch([selected, rightValue] , () => {
     width: 50%;
     // padding: 20px;
     margin-top: 20px;
-    .table{
+
+    .table {
       margin-top: 20px;
     }
   }
@@ -172,5 +209,17 @@ watch([selected, rightValue] , () => {
   .btn {
     margin-top: 10px
   }
+}
+</style>
+
+<style>
+/*.ant-message-notice{*/
+/*  display: flex;*/
+/*  align-items: center;*/
+/*  !*padding-left: 50px;*!*/
+/*}*/
+
+.ant-message-notice-content {
+  margin-left: -240px !important;
 }
 </style>
